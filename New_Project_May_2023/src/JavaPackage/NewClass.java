3749628308f299431dd9308f15b3812ab1a090b9
a <a@DESKTOP-UJAHHIE>
2023-07-13 18:11:56 +0530

    we add the code

--- New_Project_May_2023/src/JavaPackage/NewClass.java
+++ New_Project_May_2023/src/JavaPackage/NewClass.java
@@ -5,6 +5,7 @@ public class NewClass {
 	public static void main(String[] args) {
     System.out.println("selenium");
     System.out.println("komal");
+    System.out.println("BDD cucumber");
 	}
 
 }
